476fb1bb6fca44fafbf05b74aa3cc37a2ad25cec
carlos <ccastillo@geocom.com.uy>
2022-02-08 23:40:13 -0300

    Initial commit

---
+++ src/main/java/com/dondex/ddmongodb/DdMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.dondex.ddmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdMongoDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DdMongoDbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dondex/ddmongodb/DdMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dondex.ddmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdMongoDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
